45a3b88d6bd0ee31cd97476c58333e464218445e
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-16 16:11:48 +0200

    first commit

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,4 +1,30 @@
-export default function Footer() {
-    return(<footer>Footer</footer>)
+import styles from './footer.module.css'
+import grid from '../../styles/grid.module.css'
+import typography from '../../styles/typography.module.css'
+import Logo from "../logo/logo";
+import Link from "next/link";
+import {joinClassNames} from "../../utilities/componentsHelpers";
+
+export default function Footer({className}) {
+    return(
+        <footer className={joinClassNames(styles.root,className)}>
+            <div className={joinClassNames(styles.container,grid.root)}>
+                <Logo classname={styles.logo}/>
+                <ul className={joinClassNames(styles.legal,typography.etaInter500)}>
+                    <li>
+                        <Link href='/'>
+                            <a>Impresum</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/'>
+                            <a>Datenschutz</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+        </footer>
+    )
 
 }
