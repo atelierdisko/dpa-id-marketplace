323ad998baf9f01b0ad823059ff064126deb99fb
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-16 16:12:48 +0200

    change external style

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -3,13 +3,14 @@ import Logo from "../logo/logo";
 import styles from './navigation.module.css'
 import grid from '../../styles/grid.module.css'
 import typography from '../../styles/typography.module.css'
+
 import {joinClassNames} from "../../utilities/componentsHelpers";
 import Button from "../button/button";
 
 export default function Navigation({className}) {
     return(
-        <nav className={styles.root}>
-            <div className={joinClassNames(grid.root,className)}><Logo classname={styles.logo} />
+        <nav className={joinClassNames(styles.root,className)}>
+            <div className={joinClassNames(grid.root,styles.container)}><Logo classname={styles.logo} />
                 <ul className={styles.list}>
                     <li className={typography.zeta500}>
                         <Link href='#applications'>
