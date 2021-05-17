1135eb9be50b01e4724c9ae931814c9d2bb515a7
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-17 18:00:43 +0200

    merge

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 import "../components/notificationsDemo/index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
         />
         <PartnerProgram className={styles.partnerProgram} />
         <LogIn className={styles.logIn} />
-          <NotificationsDemo />
+        <NotificationsDemo />
       </main>
 
       <Footer className={joinClassNames(styles.footer)} />
