3a7437de2d6cd3967fbc0a529cefb6fa165a2287
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-17 15:51:04 +0200

    first commit

---
+++ components/slider/slider.js
@@ -0,0 +1,16 @@
+import styles from './slider.module.css'
+import typography from '../../styles/typography.module.css'
+import {joinClassNames} from "../../utilities/componentsHelpers";
+
+export default function Slider({array,activePage,className}) {
+    return(
+        <div className={joinClassNames(className,styles.root)}>
+            <div className={joinClassNames(typography.zeta500,styles.back)}>‹ Zurück</div>
+            <div className={styles.pages}>
+                {array.map((el,index)=>(<div className={joinClassNames(styles.page,activePage===(index+1) && styles.isActive)}>{}</div>))}
+            </div>
+            <div className={joinClassNames(typography.zeta500,styles.forward)}>Weiter ›</div>
+        </div>
+    )
+
+}
