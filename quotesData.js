bc3cd7f48fbc79be905f5f71aa712159a1798c73
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-18 16:12:42 +0200

    first commit

---
+++ quotesData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    signalPhrase: "Name der Person — Position der Person  ",
+    quote:
+      "„Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
+    image: "../images/quotes-image.png",
+  },
+  {
+    signalPhrase: " 1 Name der Person — Position der Person  ",
+    quote:
+      "„ 1 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
+    image: "../images/quotes-image.png",
+  },
+  {
+    signalPhrase: " 2 Name der Person — Position der Person  ",
+    quote:
+      "„ 2 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
+    image: "../images/quotes-image.png",
+  },
+  {
+    signalPhrase: " 3 Name der Person — Position der Person  ",
+    quote:
+      "„ 3 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
+    image: "../images/quotes-image.png",
+  },
+  {
+    signalPhrase: " 4 Name der Person — Position der Person  ",
+    quote:
+      "„ 4 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
+    image: "../images/quotes-image.png",
+  },
+];
