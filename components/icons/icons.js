44d41bc090524af62b0c9b7227f3bee18e60279c
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-19 11:41:51 +0200

    add show icon

--- components/icons/icons.js
+++ components/icons/icons.js
@@ -459,5 +459,19 @@ export default function Icons({ icon, className }) {
           <circle cx="16" cy="16.0005" r="16" fill="#FD7239" />
         </svg>
       );
+    case "show":
+      return (
+        <svg
+          width="41"
+          height="44"
+          viewBox="0 0 41 44"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={className}
+        >
+          <line y1="22.5" x2="41" y2="22.5" stroke="black" />
+          <line x1="21.5" y1="2.18557e-08" x2="21.5" y2="44" stroke="black" />
+        </svg>
+      );
   }
 }
