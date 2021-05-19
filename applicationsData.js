02eb29e863719906bb06c576bed5bd751d6fe914
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-19 11:42:25 +0200

    add tags

--- applicationsData.js
+++ applicationsData.js
@@ -4,95 +4,107 @@ export default [
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaNews",
+    tags: ["aktuelles", "daten"],
   },
   {
     title: "dpa-Select",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaSelect",
+    tags: ["planung", "daten"],
   },
   {
     title: "dpa-Agenda",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaAgenda",
+    tags: ["distribution", "daten"],
   },
   {
     title: "dpa-Videoportal",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaVideoPortal",
+    tags: ["video", "audio", "visuals"],
   },
   {
     title: "dpa-Shop",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaShop",
+    tags: ["recherche", "daten"],
   },
   {
     title: "dpa-picture alliance",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "dpaPictureAlliance",
+    tags: ["aktuelles"],
   },
   {
     title: "Polit-X",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "politics",
+    tags: ["aktuelles", "distribution"],
   },
   {
     title: "BotTalk",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "botTalk",
+    tags: ["recherche", "daten"],
   },
   {
     title: "Lorem ipsum",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "genericIcon",
+    tags: ["daten"],
   },
   {
     title: "Lorem ipsum",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "genericIcon",
+    tags: ["visuals"],
   },
   {
     title: "Lorem ipsum",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "genericIcon",
+    tags: ["aktuelles"],
   },
   {
     title: "Lorem ipsum",
     excerpt: "Lorem ipsum dolor sit amet, consetetur",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.",
-    images: ["./images/img1.jpeg", "./images/img2.jpeg"],
+    images: ["./images/img0.jpg", "./images/img1.jpeg", "./images/img2.jpeg"],
     icon: "genericIcon",
+    tags: ["audio", "daten"],
   },
 ];
