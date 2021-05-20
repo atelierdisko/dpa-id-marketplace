2d81b81bde3949d089c63c2091043bba7c5376ef
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-20 18:24:20 +0200

    change text

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -8,15 +8,14 @@ export default function Hero({ className }) {
   return (
     <section className={joinClassNames(className, styles.root, grid.root)}>
       <h1 className={joinClassNames(styles.title, typography.beta500)}>
-        VAR B-1 · Ihr Schlüssel zur Nachrichtenwelt der dpa
+        Willkommen in der Welt der dpa ID!{" "}
       </h1>
       <p className={joinClassNames(styles.description, typography.delta400)}>
-        Hier steht ein kurzer Text zur Einführung und Erklärung der ID. Lorem
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
+        Die dpa ID ist Ihr persönlicher Schlüssel zum Marktplatz für
+        Nachrichtenprofis und Kommunikationsteams. Auf diesem Markptlatz bieten
+        wir Ihnen wertvolle Tools der dpa und unseren Partnern, die Ihnen Ihre
+        tägliche Arbeit erleichtern. Registrieren Sie sich jetzt kostenlos und
+        entdecken Sie neue Inhalte & Services.
       </p>
       <div className={styles.buttonContainer}>
         <Button isActive={true} isDoublePadding={true}>
