57a442feb221d2248c4594b56dc7abf6e3286026
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-21 11:55:21 +0200

    first commit

---
+++ components/legal/legal.js
@@ -0,0 +1,57 @@
+import styles from "./legal.module.css";
+import typography from "../../styles/typography.module.css";
+import Button from "../button/button";
+import { joinClassNames } from "../../utilities/componentsHelpers";
+import grid from "../../styles/grid.module.css";
+
+export default function Legal({ type, className }) {
+  return (
+    <div className={joinClassNames(styles.root, grid.root)}>
+      <div className={styles.buttonContainer}>
+        <Button isActive={false} className={styles.button} href="/">
+          ← Zurück zur Startseite
+        </Button>
+      </div>
+      <h1 className={joinClassNames(styles.title, typography.gamma500)}>
+        {type === "imprint"
+          ? "Impressum"
+          : "Datenschutzerklärung der dpa Deutsche Presse-Agentur GmbH für die Website des dpa ID Partnerprogramms"}
+      </h1>
+      <div
+        className={joinClassNames(styles.description, typography.epsilon500)}
+      >
+        <p>
+          Die Nutzung dieser Informationsseite kann mit der Verarbeitung von
+          personenbezogenen Daten verbunden sein. Damit diese Verarbeitungen für
+          Sie nachvollziehbar sind, möchten wir Ihnen mit den folgenden
+          Informationen einen Überblick zu diesen Verarbeitungen verschaffen. Um
+          eine faire Verarbeitung zu gewährleisten, möchten wir Sie außerdem
+          über Ihre Rechte nach der Europäischen Datenschutz-Grundverordnung
+          (DSGVO) und dem Bundesdatenschutzgesetz (BDSG) informieren.
+        </p>
+        <p>
+          Verantwortlich für die Datenverarbeitung ist die dpa Deutsche
+          Presse-Agentur GmbH, Mittelweg 38, 20148 Hamburg (nachfolgend auch als
+          „wir“ oder „uns“ bezeichnet).{" "}
+        </p>
+        <p>
+          Bitte beachten Sie, dass diese Informationen zum Datenschutz nur für
+          die Informationsseite des dpa ID Partnerprogramms gelten. Für die
+          Nutzung des dpa-ID Singe-Sign-On-Service gelten gesonderte
+          Informationen.
+        </p>
+        <h5>1. Allgemeine</h5>
+        <p>
+          Angaben a) Kontakt Wenn Sie Fragen oder Anregungen zu diesen
+          Informationen haben oder sich wegen der Geltendmachung Ihrer Rechte an
+          uns wenden möchten, richten Sie Ihre Anfrage bitte an: dpa Deutsche
+          Presse-Agentur GmbH{" "}
+        </p>
+        <p>Mittelweg 38</p>
+        <p> 20148 Hamburg</p>
+        <p>Telefon: +49 40 4113-0</p>
+        <p>E-Mail: info@dpa.com</p>
+      </div>
+    </div>
+  );
+}
---
+++ pages/impressum/index.js
@@ -0,0 +1,14 @@
+import Legal from "../../components/legal/legal";
+import Navigation from "../../components/navigation/navigation";
+import Footer from "../../components/footer/footer";
+import styles from "./index.module.css";
+
+export default function Index() {
+  return (
+    <>
+      <Navigation displayNavigation={false} className={styles.navigation} />
+      <Legal type="imprint" className={styles.legal} />
+      <Footer className={styles.footer} />
+    </>
+  );
+}
