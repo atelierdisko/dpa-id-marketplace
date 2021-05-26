224120cf12a4d56d0b35843d541f5fcd754d2863
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-26 15:45:43 +0200

    add id prop

--- components/message/message.js
+++ components/message/message.js
@@ -3,9 +3,9 @@ import Icons from "../icons/icons";
 import { joinClassNames } from "../../utilities/componentsHelpers";
 import typography from "../../styles/typography.module.css";
 
-export default function Message({ type, children, className }) {
+export default function Message({ type, children, className, id }) {
   return (
-    <div className={styles.root}>
+    <div className={styles.root} id={id}>
       <div className={styles.typeContainer}>
         <Icons className={styles.typeLogo} icon="dpaNewsSmall" />
         <div>{type}</div>
