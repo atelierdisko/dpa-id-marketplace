dcffef153937da9e575d8326414504c77dd3ab73
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-28 16:17:35 +0200

    add mock data and states for animation

--- components/functionalities/functionalities.js
+++ components/functionalities/functionalities.js
@@ -7,7 +7,12 @@ import Slider from "../slider/slider";
 import Message from "../message/message";
 import Logo from "../logo/logo";
 
-export default function Functionalities({ functionalities, className, id }) {
+export default function Functionalities({
+  functionalities,
+  className,
+  id,
+  messages,
+}) {
   /* states and handler for slider */
   const [functionalityTitle, setFunctionalityTitle] = useState(
     functionalities[0].title
@@ -36,12 +41,20 @@ export default function Functionalities({ functionalities, className, id }) {
   };
 
   /* states and handler for animation*/
-  const [message1, setMessage1] = useState("");
-  const [message2, setMessage2] = useState("");
-  const [message3, setMessage3] = useState("");
-  const [phoneMessages, setPhoneMessages] = useState("");
+  const [message1, setMessage1] = useState(messages[0]);
+  const [message2, setMessage2] = useState(messages[1]);
+  const [message3, setMessage3] = useState(messages[2]);
+  const [phoneMessages, setPhoneMessages] = useState([
+    messages[3],
+    messages[4],
+    messages[5],
+    messages[6],
+  ]);
 
   const handleClickAnimation = ({ target }) => {};
+
+  /* setting messages*/
+  useEffect(() => {});
 
   return (
     <section className={joinClassNames(styles.root, className)} id={id}>
@@ -86,15 +99,30 @@ export default function Functionalities({ functionalities, className, id }) {
           eine Nachricht:
         </p>
         <div className={styles.messageContainer}>
-          <Message type="Meldung" id={message1}>
-            Hier steht eine wichtige Meldung, lorem ipsum dolorum sum
-          </Message>
-          <Message type="Meldung" id={message2}>
-            Niederlage für Özdemir: Hofreiter bleibt Grünen Franktionschef
-          </Message>
-          <Message type="Termin" id={message3}>
-            Niederlage für Özdemir: Hofreiter bleibt Grünen Franktionschef
-          </Message>
+          <Message
+            type={message1.type}
+            id="message1"
+            icon={message1.icon}
+            onClick={handleClickAnimation}
+            className={styles.messageToSend}
+            content={message1.content}
+          />
+          <Message
+            type={message2.type}
+            id="message2"
+            icon={message2.icon}
+            onClick={handleClickAnimation}
+            className={styles.messageToSend}
+            content={message2.content}
+          />
+          <Message
+            type={message3.type}
+            id="message3"
+            icon={message3.icon}
+            onClick={handleClickAnimation}
+            className={styles.messageToSend}
+            content={message3.content}
+          />
         </div>
 
         <div className={styles.phoneContainer}>
@@ -109,7 +137,21 @@ export default function Functionalities({ functionalities, className, id }) {
           <div className={styles.applicationTitle}>
             <span>HEUTE</span>
           </div>
-          <div className={styles.applicationMessageContainer}>{}</div>
+          <div className={styles.applicationMessageContainer}>
+            {phoneMessages.map((message, index) => (
+              <Message
+                key={index}
+                type={message.type}
+                theme={message.theme}
+                icon={message.icon}
+                onPhone={true}
+                postingTime={message.time}
+                className={styles.messageOnPhone}
+                themeColor={message.themeColor}
+                content={message.content}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
