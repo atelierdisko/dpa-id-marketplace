9bfc0cd4c4938682294e4f47e87901fffae15095
hortenciajorquera <hortencia.pj@gmail.com>
2021-05-28 16:19:10 +0200

    adapt component to render messages on the phone

--- components/message/message.js
+++ components/message/message.js
@@ -3,16 +3,63 @@ import Icons from "../icons/icons";
 import { joinClassNames } from "../../utilities/componentsHelpers";
 import typography from "../../styles/typography.module.css";
 
-export default function Message({ type, children, className, id }) {
+export default function Message({
+  type,
+  className,
+  icon,
+  id,
+  onClick,
+  disabled,
+  onPhone,
+  theme,
+  themeColor,
+  postingTime,
+  content,
+}) {
   return (
-    <div className={styles.root} id={id}>
-      <div className={styles.typeContainer}>
-        <Icons className={styles.typeLogo} icon="dpaNewsSmall" />
-        <div>{type}</div>
+    <div
+      className={joinClassNames(styles.root, className)}
+      id={id}
+      onClick={onClick}
+    >
+      <div
+        className={joinClassNames(
+          styles.typeContainer,
+          onPhone && styles.typeContainerIsOnPhone
+        )}
+      >
+        <Icons className={styles.typeLogo} icon={icon} />
+        <div>{onPhone ? type : `Neue ${type} im`}</div>
+        {onPhone && (
+          <div
+            className={styles.diamond}
+            style={{ backgroundColor: themeColor }}
+          />
+        )}
+        {onPhone && (
+          <span className={styles.theme} style={{ color: themeColor }}>
+            {theme}
+          </span>
+        )}
+        {onPhone && <span>{postingTime}</span>}
       </div>
-      <div className={joinClassNames(typography.etaInter500, styles.content)}>
-        {children}
+      <div
+        className={joinClassNames(
+          typography.etaInter500,
+          styles.content,
+          onPhone && styles.contentIsOnPhone
+        )}
+      >
+        {content}
       </div>
+      {!onPhone && (
+        <div
+          className={joinClassNames(
+            styles.overlay,
+            disabled && styles.isDisabled
+          )}
+        />
+      )}
     </div>
   );
 }
