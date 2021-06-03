2fa09357ad74d28abb932a8bf2df5bce87111a48
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-03 15:28:28 +0200

    add tablet and mobile queries

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -4,25 +4,35 @@ import typography from "../../styles/typography.module.css";
 import Logo from "../logo/logo";
 import Link from "next/link";
 import { joinClassNames } from "../../utilities/componentsHelpers";
+import { useMediaQuery } from "react-responsive";
+import { customMedia } from "../../styles/cssExports";
 
 export default function Footer({ className }) {
+  const isTablet = useMediaQuery({ query: customMedia["--tablet"] });
+  const isMobile = useMediaQuery({ query: customMedia["--mobile"] });
   return (
-    <footer className={joinClassNames(styles.root, className)}>
-      <div className={joinClassNames(styles.container, grid.root)}>
-        <Logo classname={styles.logo} />
-        <ul className={joinClassNames(styles.legal, typography.etaInter500)}>
-          <li>
-            <Link href="/impressum">
-              <a>Impressum</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/datenshutz">
-              <a>Datenschutz</a>
-            </Link>
-          </li>
-        </ul>
+    <footer
+      className={joinClassNames(styles.root, grid.root, grid.margin, className)}
+    >
+      <div className={styles.logo}>
+        <Logo />
+        {(isTablet || isMobile) && (
+          <div className={styles.partnerProgram}>Partnerprogramm</div>
+        )}
       </div>
+
+      <ul className={joinClassNames(styles.legal, typography.etaInter500)}>
+        <li>
+          <Link href="/impressum">
+            <a>Impressum</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/datenshutz">
+            <a>Datenschutz</a>
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
