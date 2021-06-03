3b454cf183876623d5b50e9de4c3dbe6226a789c
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-03 17:02:13 +0200

    add tablet and mobile queries

--- pages/datenshutz/index.js
+++ pages/datenshutz/index.js
@@ -1,14 +1,14 @@
 import Legal from "../../components/legal/legal";
 import Navigation from "../../components/navigation/navigation";
 import Footer from "../../components/footer/footer";
-import styles from "./index.module.css";
+import styles from "../../styles/legal.module.css";
 
 export default function Index() {
   return (
     <>
       <Navigation displayNavigation={false} className={styles.navigation} />
-      <Legal type="data" className={styles.legal} />
-      <Footer className={styles.footer} />
+      <Legal type="data" />
+      <Footer />
     </>
   );
 }
