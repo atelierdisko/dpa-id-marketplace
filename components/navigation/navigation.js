c1f871ddcb4c4bca8dd605bbd748b5c06535eaf1
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-03 17:00:34 +0200

    add tablet and mobile queries

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -3,41 +3,53 @@ import Logo from "../logo/logo";
 import styles from "./navigation.module.css";
 import grid from "../../styles/grid.module.css";
 import typography from "../../styles/typography.module.css";
-
+import { useMediaQuery } from "react-responsive";
+import { customMedia } from "../../styles/cssExports";
 import { joinClassNames } from "../../utilities/componentsHelpers";
 import Button from "../button/button";
 
 export default function Navigation({ className, displayNavigation, isLight }) {
+  const isDesktop = useMediaQuery({ query: customMedia["--desktop"] });
+  const isTablet = useMediaQuery({ query: customMedia["--tablet"] });
+  const isMobile = useMediaQuery({ query: customMedia["--mobile"] });
   return (
     <nav
       className={joinClassNames(
         styles.root,
+        grid.root,
+        grid.margin,
         isLight && styles.isLight,
         className
       )}
     >
-      <div className={joinClassNames(grid.root, styles.container)}>
-        <Logo classname={styles.logo} isDark={isLight} />
+      <Logo classname={styles.logo} isDark={isLight} />
 
-        <ul
-          className={styles.list}
-          style={!displayNavigation ? { display: "none" } : null}
-        >
+      <ul
+        className={styles.list}
+        style={!displayNavigation ? { display: "none" } : null}
+      >
+        {isDesktop && (
           <li className={typography.zeta500}>
             <Link href="/#applications" scroll={true}>
               <a>Anwendungen</a>
             </Link>
           </li>
+        )}
+        {isDesktop && (
           <li className={typography.zeta500}>
             <Link href="/#functionalities">
               <a>Funktionalitäten</a>
             </Link>
           </li>
+        )}
+        {isDesktop && (
           <li className={typography.zeta500}>
             <Link href="/#partnerProgram">
               <a>Partnerprogramm</a>
             </Link>
           </li>
+        )}
+        {!isMobile && (
           <li className={typography.etaInter500}>
             <Link href="/#signIn">
               <Button className={styles.signIn}>
@@ -46,13 +58,14 @@ export default function Navigation({ className, displayNavigation, isLight }) {
               </Button>
             </Link>
           </li>
-          <li className={typography.etaInter500}>
-            <Link href="/#createId">
-              <Button isActive={true}>Jetzt dpa ID anlegen</Button>
-            </Link>
-          </li>
-        </ul>
-      </div>
+        )}
+
+        <li className={typography.etaInter500}>
+          <Link href="/#createId">
+            <Button isActive={true}>Jetzt dpa ID anlegen</Button>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
