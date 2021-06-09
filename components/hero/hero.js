83010dc9e52c739a94a1917ef8287be503395008
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-09 15:08:56 +0200

    change grid definition and content

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -6,28 +6,32 @@ import grid from "../../styles/grid.module.css";
 
 export default function Hero({ className }) {
   return (
-    <section
-      className={joinClassNames(className, styles.root, grid.gridFull)}
-    >
+    <section className={joinClassNames(className, styles.root, grid.root)}>
       <h1 className={joinClassNames(styles.title, typography.beta500)}>
-        Willkommen in der Welt der dpa ID!{" "}
+        Willkommen auf dem dpa-Marktplatz!
       </h1>
       <p className={joinClassNames(styles.description, typography.delta400)}>
-        Die dpa ID ist Ihr persönlicher Schlüssel zum Marktplatz für
-        Nachrichtenprofis und Kommunikationsteams. Auf diesem Markptlatz bieten
-        wir Ihnen wertvolle Tools der dpa und unseren Partnern, die Ihnen Ihre
-        tägliche Arbeit erleichtern. Registrieren Sie sich jetzt kostenlos und
-        entdecken Sie neue Inhalte & Services.
+        dpa bietet einen Marktplatz für Nachrichtenprofis und
+        Kommunikationsteams. Auf diesem Marktplatz ermöglichen wir Ihnen einen
+        einfachen und schnellen Zugang zu wertvollen Tools der dpa und unserer
+        Partner, die Ihnen Ihre tägliche Arbeit erleichtern. Die dpa ID ist Ihr
+        persönlicher Schlüssel zu unserem Marktplatz. Registrieren Sie sich
+        jetzt kostenfrei und entdecken neue Produkte und Services.
       </p>
 
       <Button isDoublePadding={true} className={styles.button}>
         Jetzt dpa ID anlegen und Tools testen
       </Button>
-      <div className={styles.laptopImageWrapper}>
-        <img src={"./images/hero-laptop1.png"} className={styles.laptopImage} />
-      </div>
-      <img src={"./images/App_Switcher (1).png"} className={styles.appMenu} />
-
+      <img
+        src={"./images/hero-laptop1.png"}
+        className={styles.laptopImage}
+        alt={""}
+      />
+      <img
+        src={"./images/App_Switcher (1).png"}
+        className={styles.appMenu}
+        alt={""}
+      />
     </section>
   );
 }
