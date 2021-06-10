6bbcb8e33e121c0b2180ebc3b69a2cfbcda5a577
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-10 13:28:44 +0200

    change grid

--- components/signIn/signIn.js
+++ components/signIn/signIn.js
@@ -6,25 +6,39 @@ import Logo from "../logo/logo";
 
 export default function SignIn({ className, id }) {
   return (
-    <section className={joinClassNames(className, styles.root)} id={id}>
-      <div className={joinClassNames(styles.container)}>
-        <p className={joinClassNames(typography.zeta500, styles.signalPhrase)}>
-          Der Marktplatz wartet
-        </p>
-        <h2 className={joinClassNames(typography.gamma500, styles.title)}>
-          Haben wir Ihr Interesse geweckt?
-        </h2>
-        <p className={joinClassNames(typography.delta400, styles.description)}>
-          Jetzt kostenlos dpa ID registrieren und den Marktplatz entdecken!
-        </p>
-      </div>
+    <section
+      className={joinClassNames(className, styles.root, grid.root)}
+      id={id}
+    >
+      <p className={joinClassNames(typography.zeta500, styles.signalPhrase)}>
+        Der Marktplatz wartet
+      </p>
+      <h2 className={joinClassNames(typography.gamma500, styles.title)}>
+        Haben wir Ihr Interesse geweckt?
+      </h2>
+      <p className={joinClassNames(typography.delta400, styles.description)}>
+        Jetzt kostenlos dpa ID registrieren und den Marktplatz entdecken!
+      </p>
       <div className={styles.drawingsAndCallToAction}>
         <div className={styles.callToActionButton}>
           <Logo classname={styles.logo} withName={false} />
           <div className={typography.etaInter500}>dpa ID anlegen</div>
         </div>
-        <img src={"./images/signUp-pattern.svg"} className={styles.pattern} />
+        <img
+          src={"./images/signUp-pattern.svg"}
+          className={styles.pattern}
+          alt={""}
+        />
       </div>
+      <div className={styles.callToActionButtonTabletAndMobile}>
+        <Logo classname={styles.logo} withName={false} />
+        <div className={typography.etaInter500}>dpa ID anlegen</div>
+      </div>
+      <img
+        src={"./images/BG Image.svg"}
+        className={styles.patternTabletAndMobile}
+        alt={""}
+      />
     </section>
   );
 }
