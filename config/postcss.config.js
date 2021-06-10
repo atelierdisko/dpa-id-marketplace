b76ba6ea0bc5395145fa1d15e1326a983abb15b8
schaschjan <jan-jakub@atelierdisko.de>
2021-06-10 15:20:53 +0200

    swiper container overflow fix

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
+    "postcss-normalize": {},
+    "postcss-nested": {},
+    "postcss-extend-rule": {},
     "postcss-preset-env": {
       stage: 1,
       importFrom: ["./styles/globals/variables.css"],
