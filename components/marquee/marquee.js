e76f63a9ae31c1691da7c21c150b5fbde8cefdb5
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-11 13:04:20 +0200

    change animation

--- components/marquee/marquee.js
+++ components/marquee/marquee.js
@@ -1,18 +1,14 @@
 import styles from "./marquee.module.css";
 import cn from "classnames";
+import grid from "../../styles/grid.module.css";
 
 //todo: fix animation
 export default function Marquee({ children, className }) {
-  const words = children.split(" ");
   return (
     <div className={cn(styles.root, className)}>
-      <ul className={cn(styles.content)}>
-        {words.map((word, index) => (
-          <li className={styles.word} key={index}>
-            {word}
-          </li>
-        ))}
-      </ul>
+      <div className={styles.content}>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
