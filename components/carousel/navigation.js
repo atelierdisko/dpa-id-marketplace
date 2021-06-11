15ae7464be5109a4dd32727790027f06c5ad856a
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-11 10:58:33 +0200

    add pagination numbers for mobile

--- components/carousel/navigation.js
+++ components/carousel/navigation.js
@@ -1,32 +1,51 @@
 import cn from "classnames";
 import styles from "./navigation.module.css";
 import typography from "../../styles/typography.module.css";
-import {Pagination} from "./pagination";
+import { Pagination } from "./pagination";
 
 Navigation.defaultProps = {
-    color: 'black'
+  color: "black",
+};
+
+function Navigation({
+  className,
+  slidePrev,
+  slideTo,
+  slideNext,
+  index,
+  length,
+  color,
+}) {
+  return (
+    <div
+      className={cn(
+        styles.navigation,
+        styles[`is${color.capitalize()}`],
+        className
+      )}
+    >
+      <button
+        onClick={() => slidePrev()}
+        className={cn(typography.zeta500, styles.prevButton)}
+      >
+        ‹ Zurück
+      </button>
+
+      <Pagination
+        count={length}
+        activeIndex={index}
+        slideTo={slideTo}
+        color={color}
+      />
+
+      <button
+        onClick={() => slideNext()}
+        className={cn(typography.zeta500, styles.nextButton)}
+      >
+        Weiter ›
+      </button>
+    </div>
+  );
 }
 
-function Navigation({className, slidePrev, slideTo, slideNext, index, length, color}) {
-    return (
-        <div className={cn(styles.navigation, styles[`is${color.capitalize()}`], className)}>
-            <button
-                onClick={() => slidePrev()}
-                className={cn(typography.zeta500, styles.prevButton)}
-            >
-                ‹ Zurück
-            </button>
-
-            <Pagination count={length} activeIndex={index} slideTo={slideTo} color={color}/>
-
-            <button
-                onClick={() => slideNext()}
-                className={cn(typography.zeta500, styles.nextButton)}
-            >
-                Weiter ›
-            </button>
-        </div>
-    )
-}
-
-export {Navigation}
+export { Navigation };
--- components/carousel/pagination.js
+++ components/carousel/pagination.js
@@ -1,20 +1,28 @@
 import styles from "./pagination.module.css";
-import {Bullet} from "./bullet";
+import { Bullet } from "./bullet";
 
-function Pagination({count, activeIndex, slideTo, color}) {
-    let bullets = [];
+function Pagination({ count, activeIndex, slideTo, color }) {
+  let bullets = [];
 
-    for (let i = 0; i < count; i++) {
-        bullets.push((
-            <Bullet active={i === activeIndex} onClick={() => slideTo(i+1)} color={color}/>
-        ));
-    }
+  for (let i = 0; i < count; i++) {
+    bullets.push(
+      <Bullet
+        active={i === activeIndex}
+        onClick={() => slideTo(i + 1)}
+        color={color}
+      />
+    );
+  }
 
-    return (
-        <div className={styles.root}>
-            {bullets}
-        </div>
-    )
+  return (
+    <div className={styles.root}>
+      <span className={styles.bullets}>{bullets}</span>
+      <span className={styles.numbers}>
+        <span className={styles.activePageNumber}>{activeIndex + 1}</span>
+        {` / ${count}`}
+      </span>
+    </div>
+  );
 }
 
-export {Pagination}
+export { Pagination };
