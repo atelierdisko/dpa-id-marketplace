cef6f4bbe7f71eb113b5962cdeb3c3a4bc58fe71
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-17 14:32:14 +0200

    prettier

--- pages/impressum/index.js
+++ pages/impressum/index.js
@@ -2,17 +2,18 @@ import Legal from "../../components/legal/legal";
 import Navigation from "../../components/navigation/navigation";
 import Footer from "../../components/footer/footer";
 import styles from "../../styles/legal.module.css";
+import cn from "classnames";
 
 export default function Index() {
   return (
-    <>
+    <div>
       <Navigation
         displayNavigation={false}
         iconColor={"white"}
-        className={styles.navigation}
+        className={cn(styles.navigation)}
       />
       <Legal type="imprint" />
       <Footer />
-    </>
+    </div>
   );
 }
