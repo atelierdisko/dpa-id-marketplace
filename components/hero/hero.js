b9e7f111a2d9175079a3b230524eafe66dde2b6c
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-17 18:49:58 +0200

    add animation

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -3,8 +3,17 @@ import styles from "./hero.module.css";
 import typography from "../../styles/typography.module.css";
 import grid from "../../styles/grid.module.css";
 import cn from "classnames";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
 
 export default function Hero({ className }) {
+  const speed = 0.15;
+  const startY = -100;
+  const stopY = -200;
+  const { scrollYProgress } = useViewportScroll();
+
+  const y = useTransform(scrollYProgress, [0.02, speed], [startY, stopY], {
+    clamp: true,
+  });
   return (
     <section className={cn(className, styles.root, grid.root)}>
       <h1 className={cn(styles.title, typography.beta500)}>
@@ -23,15 +32,19 @@ export default function Hero({ className }) {
         Jetzt dpa ID anlegen und Tools testen
       </Button>
       <img
-        src={"./images/hero-laptop3.png"}
+        src={"./images/hero-laptop.png"}
         className={styles.laptopImage}
         alt={""}
       />
-      <img
-        src={"./images/appswitcher_trans.png"}
-        className={styles.appMenu}
-        alt={""}
-      />
+      <motion.div className={styles.appMenu} style={{ y }}>
+        {/*{" "}*/}
+        <motion.img
+          animate={{ y: startY }}
+          transition={{ duration: 1 }}
+          src={"./images/appswitcher_trans.png"}
+          alt={""}
+        />
+      </motion.div>
     </section>
   );
 }
