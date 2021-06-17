b37e6ae606b5c36b621fe9f1f908a866882b63a4
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-17 15:39:47 +0200

    change placeholder image

--- quotesData.js
+++ quotesData.js
@@ -3,7 +3,7 @@ export default [
     signalPhrase: "Name der Person — Position der Person  ",
     quote:
       "„Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes-image.png",
+    image: "../images/quotes2-image.png",
   },
   {
     signalPhrase: " 1 Name der Person — Position der Person  ",
@@ -15,13 +15,13 @@ export default [
     signalPhrase: " 2 Name der Person — Position der Person  ",
     quote:
       "„ 2 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes-image.png",
+    image: "../images/quotes2-image.png",
   },
   {
     signalPhrase: " 3 Name der Person — Position der Person  ",
     quote:
       "„ 3 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes-image.png",
+    image: "../images/quotes2-image.png",
   },
   {
     signalPhrase: " 4 Name der Person — Position der Person  ",
