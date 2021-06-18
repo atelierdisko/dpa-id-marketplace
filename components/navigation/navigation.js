e7fceafbcc9b96746805f690543cce54b8c6ca24
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-18 15:16:25 +0200

    add hover and active styles

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -5,14 +5,16 @@ import typography from "../../styles/typography.module.css";
 import Button from "../button/button";
 import { FullLogoIcon, LogoIcon } from "../icon/icon";
 import cn from "classnames";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navigation({
   className,
   displayNavigation,
   isLight,
   iconColor,
-  activeIndex = 0,
 }) {
+  const ref = useRef();
+  const [activeIndex, setActiveIndex] = useState(null);
   const primaryNavigationLinks = [
     {
       href: "#applications",
@@ -27,9 +29,41 @@ export default function Navigation({
       text: "Funktionalitäten",
     },
   ];
+  function onScroll() {
+    if (typeof document === "undefined" && ref.current) {
+      return;
+    }
+    let index = 0;
+    let prevTop = null;
+    while (index < primaryNavigationLinks.length) {
+      const { href } = primaryNavigationLinks[index];
+      const el = document.querySelector(href);
+      const { height: navHeight } = ref.current?.getBoundingClientRect();
+      const currentTop = el.getBoundingClientRect().top - navHeight;
+      if (currentTop > 0 && prevTop < 0) {
+        setActiveIndex(Math.max(0, index - 1));
+        return;
+      }
+      prevTop = currentTop;
+      index++;
+    }
+    setActiveIndex(index > 0 && prevTop < 0 ? index - 1 : null);
+  }
+
+  useEffect(() => {
+    if (typeof document === "undefined" && ref.current) {
+      return;
+    }
+    onScroll();
+    document.addEventListener("scroll", onScroll, { passive: true });
+    return () => {
+      document.removeEventListener("scroll", onScroll, { passive: true });
+    };
+  }, [ref]);
 
   return (
     <nav
+      ref={ref}
       className={cn(
         styles.root,
         grid.root,
@@ -48,15 +82,17 @@ export default function Navigation({
         style={!displayNavigation ? { display: "none" } : null}
       >
         {primaryNavigationLinks.map((link, index) => (
-          <li
-            className={cn(
-              typography.zeta500,
-              index === activeIndex && styles.activeLink
-            )}
-            key={index}
-          >
+          <li key={index}>
             <Link href={link.href}>
-              <a>{link.text}</a>
+              <a
+                className={cn(
+                  typography.zeta500,
+                  styles.primaryLink,
+                  index === activeIndex && styles.activeLink
+                )}
+              >
+                {link.text}
+              </a>
             </Link>
           </li>
         ))}
