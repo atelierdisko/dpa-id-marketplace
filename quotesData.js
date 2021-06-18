6dac77c969d68252059d9df65def9de505caffa4
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-18 15:17:07 +0200

    change images

--- quotesData.js
+++ quotesData.js
@@ -3,30 +3,30 @@ export default [
     signalPhrase: "Name der Person — Position der Person  ",
     quote:
       "„Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes2-image.png",
+    image: "../images/quotes-image2.png",
   },
   {
     signalPhrase: " 1 Name der Person — Position der Person  ",
     quote:
       "„ 1 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes-image.png",
+    image: "../images/quotes-image2.png",
   },
   {
     signalPhrase: " 2 Name der Person — Position der Person  ",
     quote:
       "„ 2 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes2-image.png",
+    image: "../images/quotes-image2.png",
   },
   {
     signalPhrase: " 3 Name der Person — Position der Person  ",
     quote:
       "„ 3 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes2-image.png",
+    image: "../images/quotes-image2.png",
   },
   {
     signalPhrase: " 4 Name der Person — Position der Person  ",
     quote:
       "„ 4 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr”",
-    image: "../images/quotes-image.png",
+    image: "../images/quotes-image2.png",
   },
 ];
