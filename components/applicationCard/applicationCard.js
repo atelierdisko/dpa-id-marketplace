7812ac7bb500294044585aff5958dc6f5cb8b8ac
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-22 12:44:21 +0200

    undo animation and change order for desktop

--- components/applicationCard/applicationCard.js
+++ components/applicationCard/applicationCard.js
@@ -62,7 +62,7 @@ export default function ApplicationCard({
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const [style, setStyle] = useState(isOpen ? styles.visible : styles.hidden);
-  const [detailsStyle, setDetailsStyle] = useState({ display: "none" });
+  // const [detailsStyle, setDetailsStyle] = useState({ display: "none" });
 
   useEffect(() => {
     setStyle(styles.visible);
@@ -72,42 +72,7 @@ export default function ApplicationCard({
     setIsOpen(false);
   }, [filter]);
 
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     setTimeout(() => {
-  //       setDetailsStyle({ display: "none" });
-  //     }, 550);
-  //   } else {
-  //     setTimeout(() => {
-  //       setDetailsStyle({ display: "grid" });
-  //     }, 550);
-  //   }
-  // }, [isOpen]);
-
-  // const variants = {
-  //   hidden: { opacity: 0, scale: 0 },
-  //   visible: {
-  //     opacity: 1,
-  //     scale: 1,
-  //     transition: {
-  //       // delay: 0.3 * index,
-  //       duration: 0.1,
-  //     },
-  //   },
-  //   // exit: {
-  //   //   opacity: 0,
-  //   //   scale: 0,
-  //   // },
-  //   opened: {},
-  // };
-
-  // const controls = useAnimation();
-  // const variantsDetails = {
-  //   visible: { opacity: 1, transition: { duration: 1 } },
-  // };
-  // useEffect(() => {
-  //   controls.start("visible");
-  // }, [isOpen]);
+  console.log(delayIndex);
 
   return (
     <>
@@ -116,11 +81,6 @@ export default function ApplicationCard({
         style={Object.assign(isOpen ? { backgroundColor: "#eeeeee" } : {}, {
           transitionDelay: `${100 * delayIndex}ms`,
         })}
-        // style={isOpen ? { backgroundColor: "#eeeeee" } : null}
-        // variants={variants}
-        // initial={{ opacity: 0, scale: 0 }}
-        // annimate={{ opacity: 1, scale: 1 }}
-        // exit={{ opacity: 0, scale: 0 }}
       >
         <div className={styles.header} onClick={() => setIsOpen(!isOpen)}>
           <div className={styles.iconContainer}>{icon}</div>
@@ -138,53 +98,56 @@ export default function ApplicationCard({
             <Icon Component={CaretDownIcon} />
           </button>
         </div>
+        {/*</div>*/}
+
+        {isOpen && (
+          // <div
+          //   className={cn(styles.details, isOpen && styles.isOpenedDetails)}
+          //   // style={detailsStyle}
+          // >
+          <>
+            <p
+              className={cn(
+                typography.epsilon400,
+                styles.excerptMobile,
+                isOpen && styles.isVisibleDetails
+              )}
+            >
+              {excerpt}
+            </p>
+
+            <div
+              className={cn(
+                styles.description,
+                typography.epsilon400,
+                isOpen && styles.isVisibleDetails
+              )}
+            >
+              {description}
+            </div>
+
+            <Button
+              isActive={true}
+              isDoublePadding={true}
+              className={cn(styles.button, isOpen && styles.isVisibleDetails)}
+              isBlue={true}
+            >
+              {`Jetzt ${title} testen`}
+            </Button>
+
+            <Carousel
+              classname={cn(styles.carousel, isOpen && styles.isVisibleDetails)}
+            >
+              {images.map((image, index) => (
+                <SwiperSlide className={styles.carouselSlide}>
+                  <img src={image} alt={""} key={index} />
+                </SwiperSlide>
+              ))}
+            </Carousel>
+            {/*</div>*/}
+          </>
+        )}
       </div>
-
-      {/*{isOpen && (*/}
-      <div
-        className={cn(styles.details, isOpen && styles.isOpenedDetails)}
-        style={detailsStyle}
-      >
-        <p
-          className={cn(
-            typography.epsilon400,
-            styles.excerptMobile,
-            isOpen && styles.isVisibleDetails
-          )}
-        >
-          {excerpt}
-        </p>
-
-        <div
-          className={cn(
-            styles.description,
-            typography.epsilon400,
-            isOpen && styles.isVisibleDetails
-          )}
-        >
-          {description}
-        </div>
-
-        <Button
-          isActive={true}
-          isDoublePadding={true}
-          className={cn(styles.button, isOpen && styles.isVisibleDetails)}
-          isBlue={true}
-        >
-          {`Jetzt ${title} testen`}
-        </Button>
-
-        <Carousel
-          classname={cn(styles.carousel, isOpen && styles.isVisibleDetails)}
-        >
-          {images.map((image, index) => (
-            <SwiperSlide className={styles.carouselSlide}>
-              <img src={image} alt={""} key={index} />
-            </SwiperSlide>
-          ))}
-        </Carousel>
-      </div>
-      {/*)}*/}
     </>
   );
 }
