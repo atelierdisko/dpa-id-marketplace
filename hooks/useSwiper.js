dcf08a3feeb50c6d9806b9a7a1e88f92ce1f2db9
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-25 18:45:18 +0200

    add controller and effectFade

--- hooks/useSwiper.js
+++ hooks/useSwiper.js
@@ -1,45 +1,60 @@
 import "swiper/swiper.min.css";
+import "swiper/components/effect-fade/effect-fade.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import SwiperCore, {Pagination as SwiperPagination, Mousewheel, Keyboard} from "swiper/core";
-import {useState} from "react";
+import SwiperCore, {
+  Pagination as SwiperPagination,
+  Mousewheel,
+  Keyboard,
+  EffectFade,
+  Controller,
+} from "swiper/core";
+import { useState } from "react";
 
-SwiperCore.use([SwiperPagination, Mousewheel, Keyboard]);
+SwiperCore.use([
+  SwiperPagination,
+  Mousewheel,
+  Keyboard,
+  EffectFade,
+  Controller,
+]);
 
 function useSwiper() {
-    const [swiper, setSwiper] = useState(null);
-    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [swiper, setSwiper] = useState(null);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-    const slideNext = () => {
-        if (swiper !== null) {
-            swiper.slideNext();
-        }
-    };
-
-    const slidePrev = () => {
-        if (swiper !== null) {
-            swiper.slidePrev();
-        }
-    };
-
-    const slideTo = (index)=>{
-        if (swiper !== null) {
-            swiper.slideTo(index);
-        }
+  const slideNext = () => {
+    if (swiper !== null) {
+      swiper.slideNext();
     }
+  };
 
-    const onSlideChange = (swiper) => {
-        setCurrentSlideIndex(swiper.realIndex);
-    };
-
-    return {
-        setSwiper,
-        slidePrev,
-        slideNext,
-        slideTo,
-        onSlideChange,
-        currentSlideIndex
+  const slidePrev = () => {
+    if (swiper !== null) {
+      swiper.slidePrev();
     }
+  };
+
+  const slideTo = (index) => {
+    if (swiper !== null) {
+      swiper.slideTo(index);
+    }
+  };
+
+  const onSlideChange = (swiper) => {
+    setCurrentSlideIndex(swiper.realIndex);
+    console.log(swiper.realIndex);
+  };
+
+  return {
+    swiper,
+    setSwiper,
+    slidePrev,
+    slideNext,
+    slideTo,
+    onSlideChange,
+    currentSlideIndex,
+  };
 }
 
-export {useSwiper}
+export { useSwiper };
