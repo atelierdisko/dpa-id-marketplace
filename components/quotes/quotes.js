9bf27bf5e207b8f8751dbe54ba5445b05d3918e0
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-28 11:31:17 +0200

    add swiper to quote

--- components/quotes/quotes.js
+++ components/quotes/quotes.js
@@ -6,49 +6,86 @@ import cn from "classnames";
 import { Navigation } from "../carousel/navigation";
 import { useSwiper } from "../../hooks/useSwiper";
 import quotes from "../../data/quotes";
+import { useState } from "react";
 
 export default function Quotes({}) {
   const {
-    setSwiper,
+    swiper: imageSwiper,
+    setSwiper: setImageSwiper,
     slidePrev,
     slideNext,
     onSlideChange,
     slideTo,
     currentSlideIndex,
   } = useSwiper();
+  const [signalPhraseSwiper, setSignalPhraseSwiper] = useState(null);
+  const [quoteSwiper, setQuoteSwiper] = useState(null);
 
   return (
     <div className={cn(styles.root, grid.root)} id={"best-practices"}>
       <Swiper
         loop={true}
         keyboard={true}
-        onSwiper={setSwiper}
+        onSwiper={setImageSwiper}
         onSlideChange={onSlideChange}
-        className={styles.carouselContainer}
+        className={styles.imageContainer}
+        controller={{ control: quoteSwiper }}
       >
         {quotes.map((quote, index) => (
-          <SwiperSlide key={index} className={styles.carouselSlide}>
+          <SwiperSlide key={index} className={styles.imageSlide}>
             <img src={quote.image} alt={""} />
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.textContainer}>
+        <p className={cn(typography.zeta500, styles.signalPhrase)}>
+          {quotes[currentSlideIndex].signalPhrase}
+        </p>
+        {/*<Swiper*/}
+        {/*  loop={true}*/}
+        {/*  keyboard={true}*/}
+        {/*  onSwiper={setQuoteSwiper}*/}
+        {/*  onSlideChange={onSlideChange}*/}
+        {/*  className={styles.signalPhraseContainer}*/}
+        {/*  // controller={{ control: quoteSwiper }}*/}
+        {/*>*/}
+        {/*  {quotes.map((quote, index) => (*/}
+        {/*    <SwiperSlide key={index} className={styles.styles.signalPhraseSlide}>*/}
+        {/*      <p className={cn(typography.zeta500)}>*/}
+        {/*        {quote.signalPhrase}*/}
+        {/*      </p>*/}
+        {/*    </SwiperSlide>*/}
+        {/*  ))}*/}
+        {/*</Swiper>*/}
 
-      <p className={cn(typography.zeta500, styles.signalPhrase)}>
-        {quotes[currentSlideIndex].signalPhrase}
-      </p>
-      <p className={cn(typography.gamma500, styles.quote)}>
-        {quotes[currentSlideIndex].quote}
-      </p>
-
-      <Navigation
-        className={styles.carouselNavigation}
-        index={currentSlideIndex}
-        length={quotes.length}
-        slideTo={slideTo}
-        slideNext={slideNext}
-        slidePrev={slidePrev}
-        color={"white"}
-      />
+        <Swiper
+          loop={true}
+          keyboard={true}
+          onSwiper={setQuoteSwiper}
+          onSlideChange={onSlideChange}
+          className={styles.quoteContainer}
+          controller={{ control: imageSwiper }}
+          effect={"fade"}
+          fadeEffect={{
+            crossFade: true,
+          }}
+        >
+          {quotes.map((quote, index) => (
+            <SwiperSlide key={index} className={styles.quoteSlide}>
+              <p className={cn(typography.gamma500)}>{quote.quote}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Navigation
+          className={styles.carouselNavigation}
+          index={currentSlideIndex}
+          length={quotes.length}
+          slideTo={slideTo}
+          slideNext={slideNext}
+          slidePrev={slidePrev}
+          color={"white"}
+        />
+      </div>
     </div>
   );
 }
