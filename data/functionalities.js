e23b83a2aa1575b25397801333ca34dbea5fb9bc
hortenciajorquera <hortencia.pj@gmail.com>
2021-06-29 12:01:49 +0200

    disable slider in features

--- data/functionalities.js
+++ data/functionalities.js
@@ -4,14 +4,14 @@ export default [
     description:
       "In der heutigen Welt sind relevante Informationen immer eine Art persönliche Eilmeldung – deshalb muss ich sie auch unterwegs sofort mitbekommen. Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen.In der heutigen Welt sind relev ",
   },
-  {
-    title: "second title ",
-    description:
-      "deshalb muss ich sie auch unterwegs sofort mitbekommen. Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen. ",
-  },
-  {
-    title: "other thing",
-    description:
-      "relevante Informationen immer eine Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen. ",
-  },
+  // {
+  //   title: "second title ",
+  //   description:
+  //     "deshalb muss ich sie auch unterwegs sofort mitbekommen. Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen. ",
+  // },
+  // {
+  //   title: "other thing",
+  //   description:
+  //     "relevante Informationen immer eine Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen. ",
+  // },
 ];
