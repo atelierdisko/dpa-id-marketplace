5282475be12b98a3530282b231ca342be9130a6e
hortenciajorquera <hortencia.pj@gmail.com>
2021-08-03 12:31:47 +0200

    update images in quotes component

--- data/quotes.js
+++ data/quotes.js
@@ -1,32 +1,26 @@
 export default [
   {
-    signalPhrase: "Name der Person — Position der Person  ",
-    quote:
-      "Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr Hier steht ein kurzer Text mit einem Zitat.Hier steht ein kurzer Text mit einem Zitat.",
-    image: "../images/quotes/update.jpg",
-  },
-  {
     signalPhrase: " 1 Name der Person — Position der Person  ",
     quote:
       "1 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
-    image: "../images/quotes/3update-1.jpg",
+    image: "../images/quotes/DPAID_Illu1_small.jpg",
   },
   {
     signalPhrase: " 2 Name der Person — Position der Person  ",
     quote:
       "2 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
-    image: "../images/quotes/quotes-image4.jpeg",
+    image: "../images/quotes/DPAID_Illu2_small.jpg",
   },
   {
     signalPhrase: " 3 Name der Person — Position der Person  ",
     quote:
       "3 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
-    image: "../images/quotes/quotes-image2.png",
+    image: "../images/quotes/DPAID_Illu3_small.jpg",
   },
   {
     signalPhrase: " 4 Name der Person — Position der Person  ",
     quote:
       "4 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
-    image: "../images/quotes/quotes-image3.jpeg",
+    image: "../images/quotes/DPAID_Illu4_small.jpg",
   },
 ];
