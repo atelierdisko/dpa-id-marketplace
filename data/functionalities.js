0c3590dfce607438cc9e6dc395b9684fa1adcf5d
hortenciajorquera <hortencia.pj@gmail.com>
2021-08-11 18:17:31 +0200

    fix text in functionalities section

--- data/functionalities.js
+++ data/functionalities.js
@@ -2,7 +2,7 @@ export default [
   {
     title: "dpa ID App – Ihr persönlicher Informationsassistent ",
     description:
-      "In der heutigen Welt sind relevante Informationen immer eine Art persönliche Eilmeldung – deshalb muss ich sie auch unterwegs sofort mitbekommen. Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen.In der heutigen Welt sind relev ",
+      "In der heutigen Welt sind relevante Informationen immer eine Art persönliche Eilmeldung – deshalb muss ich sie auch unterwegs sofort mitbekommen. Mit der dpa ID App können Sie sich über alle für Sie relevanten neuen Inhalte der Services dpa-Agenda und dpa-Select benachrichtigen lassen.",
   },
   // {
   //   title: "second title ",
