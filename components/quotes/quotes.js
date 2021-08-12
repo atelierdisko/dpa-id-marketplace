f120d324b3d8f9967fa86f2ccdc83d2efc25d2f9
hortenciajorquera <hortencia.pj@gmail.com>
2021-08-12 13:16:57 +0200

    change layout of quotes section

--- components/quotes/quotes.js
+++ components/quotes/quotes.js
@@ -1,97 +1,79 @@
 import styles from "./quotes.module.css";
 import typography from "../../styles/typography.module.css";
 import grid from "../../styles/grid.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 import cn from "classnames";
-import { Navigation } from "../carousel/navigation";
-import { useSwiper } from "../../hooks/useSwiper";
+import {Navigation} from "../carousel/navigation";
+import {useSwiper} from "../../hooks/useSwiper";
 import quotes from "../../data/quotes";
-import { useState } from "react";
+import {useState} from "react";
 
 export default function Quotes({}) {
-  const {
-    swiper: imageSwiper,
-    setSwiper: setImageSwiper,
-    slidePrev,
-    slideNext,
-    onSlideChange,
-    slideTo,
-    currentSlideIndex,
-  } = useSwiper();
-  const [quoteSwiper, setQuoteSwiper] = useState(null);
-  const [signalPhraseSwiper, setSignalPhraseSwiper] = useState(null);
+    const {
+        swiper: imageSwiper,
+        setSwiper: setImageSwiper,
+        slidePrev,
+        slideNext,
+        onSlideChange,
+        slideTo,
+        currentSlideIndex,
+    } = useSwiper();
+    const [quoteSwiper, setQuoteSwiper] = useState(null);
 
-  return (
-    <div className={cn(styles.root, grid.root)} id={"best-practices"}>
-      <Swiper
-        loop={true}
-        keyboard={true}
-        onSwiper={setImageSwiper}
-        onSlideChange={onSlideChange}
-        className={styles.imageContainer}
-        controller={{
-          control:
-            signalPhraseSwiper && quoteSwiper
-              ? [signalPhraseSwiper, quoteSwiper]
-              : null,
-        }}
-      >
-        {quotes.map((quote, index) => (
-          <SwiperSlide key={index} className={styles.imageSlide}>
-            <img src={quote.image} alt={""} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div className={styles.textContainer}>
-        <Swiper
-          loop={true}
-          onSwiper={(swiper) => {
-            setSignalPhraseSwiper(swiper);
-          }}
-          className={styles.signalPhraseContainer}
-          controller={{
-            control: imageSwiper,
-          }}
-          effect={"fade"}
-          fadeEffect={{
-            crossFade: true,
-          }}
-        >
-          {quotes.map((quote, index) => (
-            <SwiperSlide key={index} className={styles.signalPhraseSlide}>
-              <p className={cn(typography.zeta500)}>{quote.signalPhrase}</p>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    return (
 
-        <Swiper
-          loop={true}
-          onSwiper={(swiper) => {
-            setQuoteSwiper(swiper);
-          }}
-          className={styles.quoteContainer}
-          controller={{ control: imageSwiper }}
-          effect={"fade"}
-          fadeEffect={{
-            crossFade: true,
-          }}
-        >
-          {quotes.map((quote, index) => (
-            <SwiperSlide key={index} className={styles.quoteSlide}>
-              <p className={cn(typography.gamma500)}>{quote.quote}</p>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <Navigation
-          className={styles.carouselNavigation}
-          index={currentSlideIndex}
-          length={quotes.length}
-          slideTo={slideTo}
-          slideNext={slideNext}
-          slidePrev={slidePrev}
-          color={"white"}
-        />
-      </div>
-    </div>
-  );
+        <div className={cn(styles.root, grid.root)} id={"best-practices"}>
+            <div className={cn(styles.titleWrapper,grid.root)}>
+                <div className={styles.block}/>
+                <h1 className={cn(styles.title, typography.beta500)}>Arbeiten mit dem dpa-Marketplace</h1>
+            </div>
+            <Swiper
+                loop={true}
+                keyboard={true}
+                onSwiper={setImageSwiper}
+                onSlideChange={onSlideChange}
+                className={styles.imageContainer}
+                controller={{
+                    control: quoteSwiper
+                }}
+            >
+                {quotes.map((quote, index) => (
+                    <SwiperSlide key={index} className={styles.imageSlide}>
+                        <img src={quote.image} alt={""}/>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <div className={styles.textContainer}>
+                <Swiper
+                    loop={true}
+                    onSwiper={(swiper) => {
+                        setQuoteSwiper(swiper);
+                    }}
+                    className={styles.quoteContainer}
+                    controller={{control: imageSwiper}}
+                    effect={"fade"}
+                    fadeEffect={{
+                        crossFade: true,
+                    }}
+                >
+                    {quotes.map((quote, index) => (
+                        <SwiperSlide key={index} className={styles.quoteSlide}>
+                            <p className={cn(typography.delta400)}>{quote.quote}</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <Navigation
+                    className={styles.carouselNavigation}
+                    index={currentSlideIndex}
+                    length={quotes.length}
+                    slideTo={slideTo}
+                    slideNext={slideNext}
+                    slidePrev={slidePrev}
+                    color={"black"}
+                    paginationColor={'gray'}
+                />
+            </div>
+
+        </div>
+    );
 }
