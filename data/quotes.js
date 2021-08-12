cacf85f50437132a5678d4fc87d87e73ea062c72
hortenciajorquera <hortencia.pj@gmail.com>
2021-08-12 13:15:00 +0200

    update text in quotes section

--- data/quotes.js
+++ data/quotes.js
@@ -2,25 +2,25 @@ export default [
   {
     signalPhrase: " 1 Name der Person — Position der Person  ",
     quote:
-      "1 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+      "Ob redaktionelle Texte oder werbliche Broschüren: Auf dem dpa-Marketplace finden Sie nützliche Services für Ihre Content-Erstellung.",
     image: "../images/quotes/DPAID_Illu1_small.jpg",
   },
   {
     signalPhrase: " 2 Name der Person — Position der Person  ",
     quote:
-      "2 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+      "Von Podcast bis Voice-Assistant: Mit unseren Marktplatz-Tools erstellen Sie Ihr individuelles Audio-Angebot.",
     image: "../images/quotes/DPAID_Illu2_small.jpg",
   },
   {
     signalPhrase: " 3 Name der Person — Position der Person  ",
     quote:
-      "3 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+      "Immer informiert! Die dpa ID App ist der persönliche Informationsassistent und pusht relevante Themen direkt auf Ihr Smartphone.r",
     image: "../images/quotes/DPAID_Illu3_small.jpg",
   },
   {
     signalPhrase: " 4 Name der Person — Position der Person  ",
     quote:
-      "4 Hier steht ein kurzer Text mit einem Zitat. Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+      "Video bewegt: Wir unterstützen Ihre Videoproduktion mit sendefertigen Beiträgen und Rohmaterial zu allen Topthemen in Deutschland.",
     image: "../images/quotes/DPAID_Illu4_small.jpg",
   },
 ];
